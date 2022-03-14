49427eb058e28d22fda1722bd63ee8942a09cb43
bananatina <101440444+bananatina@users.noreply.github.com>
2022-03-13 20:54:55 -0600

    Add files via upload
    刪除資料庫地址為空後修復總數不相符問題

--- index_details.py
+++ index_details.py
@@ -42,40 +42,59 @@ def city(results):
     city_dic = dict()
     city_cost_dic=dict()
     city_detail=list()
+    count=0
     for arr in results:
         # tuples for all query
         addr_city=arr[8].split()
         if arr[11] != '':
             addr_cost=arr[11].split('$')
         else:
-            continue
+            addr_cost=0
         # print(addr_city)
         # print(len(addr_city))
         # print(addr_cost)
         addrlong=len(addr_city)
         
         # print(addr_city[addrlong-2])
-        if addr_city[addrlong-2] not in city_dic:
-            city_dic[addr_city[addrlong-2]]=1
-            # print(addr_cost[len(addr_cost)-1])
-            if addr_cost[len(addr_cost)-1]!= '':
+        if addr_cost !=0:
+            if addr_city[addrlong-2] not in city_dic:
+                city_dic[addr_city[addrlong-2]]=1
+                # print(addr_cost[len(addr_cost)-1])
+                # if addr_cost[len(addr_cost)-1]!= '':
+                #     city_cost_dic[addr_city[addrlong-2]]=int(addr_cost[len(addr_cost)-1])
+                # else:
+                #     continue
                 city_cost_dic[addr_city[addrlong-2]]=int(addr_cost[len(addr_cost)-1])
+
             else:
-                continue
-        else:
-            city_dic[addr_city[addrlong-2]]=city_dic[addr_city[addrlong-2]]+1
-            if addr_cost[len(addr_cost)-1]!= '':
+                city_dic[addr_city[addrlong-2]]=city_dic[addr_city[addrlong-2]]+1
+                # if addr_cost[len(addr_cost)-1]!= '':
+                #     city_cost_dic[addr_city[addrlong-2]]=city_cost_dic[addr_city[addrlong-2]]+int(addr_cost[len(addr_cost)-1])
+                # else:
+                #     continue
                 city_cost_dic[addr_city[addrlong-2]]=city_cost_dic[addr_city[addrlong-2]]+int(addr_cost[len(addr_cost)-1])
+
+            # count+=1
+            # print(count)
+        else:
+            # print(city_cost_dic)
+            # count+=1
+            # print(count)
+            # print(addr_city)
+            if addr_city[addrlong-2] not in city_dic:
+                city_dic[addr_city[addrlong-2]]=1
+                city_cost_dic[addr_city[addrlong-2]]=0
+
             else:
-                continue
+                city_dic[addr_city[addrlong-2]]=city_dic[addr_city[addrlong-2]]+1
+                city_cost_dic[addr_city[addrlong-2]]=city_cost_dic[addr_city[addrlong-2]]+0
     # print(city_dic)
     # print(city_cost_dic)
     for city in city_dic:
         # print(type(city_cost_dic['Taipei']))
         # print(city_dic.get(city,0))
         # print(city[0],city[1],int(city_cost_dic[city[0]])/int(city[1]))
-        city_detail.append([city,city_dic.get(city,0),round(int(city_cost_dic[city])/city_dic.get(city,0),0)])
+        city_detail.append([city,city_dic.get(city,0),round(int(city_cost_dic[city])/city_dic.get(city,0),0)])    
     return city_detail
 
-
-database()
+# database()
