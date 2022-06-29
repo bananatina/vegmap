9474f51b3a809c37c933c4ad75a0b42a655780d4
Tîng-huē Tân <71164837+hejkeikei@users.noreply.github.com>
2022-06-29 17:31:03 -0600

    2022-06-29 update
    - Replace all PHP with JavaScript
    - Replace links parameters

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
+var path = window.location.pathname;
+var page = path.split("/").pop();
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const map = urlParams.get('map')
+const restaurantList = document.getElementById("restaurantList");
+
 function showinfo(name){
     info.style.display="block";
     document.querySelector("#info h3").innerHTML=name;
@@ -18,5 +25,27 @@ for(let i of maplist){
     })
 }
 filter.addEventListener("click",()=>{filterbox.classList.toggle("open")})
-mode.addEventListener("click",()=>{restaurantList.classList.toggle("open")})
 
+function createRestaurantElem(){
+    const list = document.querySelector("#restaurantList ul");
+    const newLi = document.createElement("li");
+    const imgBox = document.createElement("div");
+    imgBox.classList.add("imgBox");
+    newLi.appendChild(imgBox);
+    imgBox.style.backgroundImage="url(http://picsum.photos/300/200)";
+    const restaurantElemBody = document.createElement("div");
+    restaurantElemBody.classList.add("restaurantElem");
+    let textBlock = ["Diet Type","Menu Type","Contact","Address","Transportation","Business Hours","Average Cost","Website"];
+    for(let i of textBlock){
+        const restaurantInfo = document.createElement("div");
+        restaurantInfo.classList.add("restaurantInfo");
+        const h5 = document.createElement("h5");
+        h5.innerHTML= i;
+        restaurantInfo.appendChild(h5);
+        restaurantElemBody.appendChild(restaurantInfo);
+    }
+    newLi.appendChild(restaurantElemBody);
+    list.appendChild(newLi);
+
+
+}
