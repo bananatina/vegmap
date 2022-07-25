e9ae3a95a6d073aef988f3fc5aa53fb9abc4c91d
Tîng-huē Tân <71164837+hejkeikei@users.noreply.github.com>
2022-07-24 18:41:26 -0600

    form upadate

---
+++ formhandle.js
@@ -0,0 +1,9 @@
+const inquery =  document.getElementById("requestType");
+const formNewShop = document.getElementById("formNewShop");
+inquery.addEventListener("change",()=>{
+    if(inquery.value==="newShop"){
+        formNewShop.classList.remove("hidden");
+    }else{
+        formNewShop.classList.add("hidden");
+    }
+})
