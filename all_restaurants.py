d67ba687ce72d856885a9e05b5a036a27ec45807
Tina <yintingwang0209@gmail.com>
2022-09-07 09:06:44 -0600

    2022/09/7 *development mode changes:   1.working on split address and keep the city amount upload   1. todo:   1.modify the address into requested number

--- all_restaurants.py
+++ all_restaurants.py
@@ -12,7 +12,7 @@ def all_restaurants(app):
 def restaurant_city(addr):
     # const damiDataStat = [156,35,25,126,79,85,3,10,15,36,45,11,2,49,33,21,16,75,18,19,32,21]
     # const cities = ["Kaohsiung City","Pingtung","Tainan City","Hsinchu City","Hsinchu","Yilan","Keelung City","Miaoli","Taipei City","New Taipei City","Taoyuan","Changhua","Chiayi","Chiayi City","Hualien","Nantou","Taichung City","Yunlin","Taitung","Penghu","Kinmen","Lienchiang"];
-    print (addr)
+    # print (addr)
     city_amount_list=dict()
     # fullstring = "StackAbuse" substring = "tack" if substring in fullstring:     print("Found!") else:     print("Not found!")
     city_compaire=["Kaohsiung City","Pingtung","Tainan City","Hsinchu City","Hsinchu","Yilan","Keelung City","Miaoli","Taipei City","New Taipei City","Taoyuan","Changhua","Chiayi","Chiayi City","Hualien","Nantou","Taichung City","Yunlin","Taitung","Penghu","Kinmen","Lienchiang"]
@@ -30,21 +30,37 @@ def restaurant_city(addr):
     #                 city_amount_list[substring]=city_amount_list[substring]+1
     #         else:
     #             print("F")
-
-    substring="Keelung City"
-    for city_addr in addr:
-        if substring in city_addr:
-            print("substring in city addr")
-            if city_amount_list[substring] == NULL:
-                print("substring is null")
-                city_amount_list[substring]=1
+    # print(type(addr))
+    for row in addr:
+        for city_addr in row:
+            # print(type(city_addr))
+            # print(city_addr.split(",",-1))
+            col_city_addr=city_addr.split(", ",-1)
+            len_city_addr=len(col_city_addr)
+            city=col_city_addr[len_city_addr-1]
+            if city not in city_amount_list:
+                city_amount_list[city]=1
             else:
-                print("substring is not null")
+                city_amount_list[city]=city_amount_list[city]+1
 
-                city_amount_list[substring]=city_amount_list[substring]+1
-        else:
-            print("F")
     print(city_amount_list)
+    # substring="keelung city"
+    # for city_addr in addr:
+    #
+    #     addr_city_cut=city_addr.split()
+    #     addr_city=addr_city_cut[len(addr_city_cut)]
+    #     if substring.fullmatch(addr_city.lower()):
+    #         print("substring in city addr")
+    #         if substring not in city_amount_list:
+    #             print("substring is null")
+    #             city_amount_list[substring]=1
+    #         else:
+    #             print("substring is not null")
+    #
+    #             city_amount_list[substring]=city_amount_list[substring]+1
+    #     else:
+    #         print("F")
+    # print(city_amount_list)
         # if city[address_len-2] not in city_amount_list:
 
     return city_amount_list
