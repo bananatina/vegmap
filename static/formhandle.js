96e5fbc2c183931b6759ef803e1aa12cd1d6314f
Tîng-huē Tân <71164837+hejkeikei@users.noreply.github.com>
2023-01-14 11:26:22 -0700

    form style and control

--- static/formhandle.js
+++ static/formhandle.js
@@ -1,9 +1,9 @@
-const inquery =  document.getElementById("requestType");
+const inquery = document.getElementById("requestType");
 const formNewShop = document.getElementById("formNewShop");
-inquery.addEventListener("change",()=>{
-    if(inquery.value==="newShop"){
+inquery.addEventListener("change", () => {
+    if (inquery.value === "newShop") {
         formNewShop.classList.remove("hidden");
-    }else{
+    } else {
         formNewShop.classList.add("hidden");
     }
 })
